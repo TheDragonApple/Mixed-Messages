2c9fd4494e467b34ff3e29cf75b1f529c92ba28e
Toby <avtrench@gmail.com>
2024-06-12 14:13:50 -0400

    Add messages

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("poop world");
+let messageContainer = {
+    entrees: ['steak', 'turkey melt', 'salmon', 'crispy chicken salad', 'chicken tenders', 'burger'],
+    sides: ['french fries', 'apple sauce', 'onion rings', 'no side', 'coleslaw', 'mac n cheese'],
+    beverages: ['water', 'coke', 'apple juice', 'vanilla milkshake', 'water with lemon', 'diet soda'],
+    gettingDessert: false,
+    desserts: ['hot fudge sundae', 'brownie', 'cookies', 'super sundae', 'ice cream cone', 'chocolate milkshake'],
+    leavingRating: false,
+    ratings: ['you enjoyed it!', 'you hated it', 'you thought it was alright', 'you need a second opinion', 'you would recomend it to a friend', 'you would never eat here again']
+};
